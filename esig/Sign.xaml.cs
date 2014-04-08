ec7c76dcc24171b929441ca45fb16b176bf7668a
Jake Crane <jakecrane91@gmail.com>
2014-04-08 18:17:42 -0500

    Moved TimedPoint class to seperate file.

--- esig/Sign.xaml.cs
+++ esig/Sign.xaml.cs
@@ -16,26 +16,6 @@ namespace PhoneApp9 {
 
     public partial class MainPage : PhoneApplicationPage {
 
-        public class TimedPoint {
-
-            public double X {get; set;}
-
-            public double Y {get; set;}
-
-            public long Time {get; set;}
-
-            public TimedPoint(double x, double y) {
-                X = x;
-                Y = y;
-                Time = (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
-            }
-
-            public override string ToString() {
-                return "{\"x\":" + X + ",\"y\":" + Y + ",\"time\":" + Time + "}";
-            }
-
-        }
-
         string sigId;
         string site;
 
---
+++ esig/TimedPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class TimedPoint {
+
+    public double X { get; set; }
+
+    public double Y { get; set; }
+
+    public long Time { get; set; }
+
+    public TimedPoint(double x, double y) {
+        X = x;
+        Y = y;
+        Time = (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+    }
+
+    public override string ToString() {
+        return "{\"x\":" + X + ",\"y\":" + Y + ",\"time\":" + Time + "}";
+    }
+
+}
